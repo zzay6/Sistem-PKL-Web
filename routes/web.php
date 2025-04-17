82d2e697e04e65c87d55e91f8b52bc85eed99512
zzay <zacky29gaming@gmail.com>
2025-04-17 22:03:39 +0700

    Route

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,32 @@ Route::get('/perusahaan', function () {
     return view('perusahaan');
 });
 
+Route::get('/buat-perusahaan', function () {
+    return view('buat-perusahaan');
+});
+
 Route::get('/data-siswa', function () {
     return view('data-siswa');
+});
+
+Route::get('/tambah-siswa', function () {
+    return view('perusahaan');
 });
 
 Route::get('/admin', function () {
     return view('admin');
 });
 
+Route::get('/tambah-admin', function () {
+    return view('tambah-admin');
+});
+
 Route::get('/lowongan', function () {
     return view('lowongan');
+});
+
+Route::get('/buat-lowongan', function () {
+    return view('buat-lowongan');
 });
 
 Route::get('/lamaran', function () {
