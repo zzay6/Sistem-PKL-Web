5148ebc1d52aba0c2e74920913910fc8efa3cc10
afif <afifahmad121@gmail.com>
2025-04-17 22:19:41 +0700

    sdsd

---
+++ resources/views/tambah-siswa.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.app')
+@section("content")
+<main>
+                    <div class="container-fluid px-4">
+                        <h1 class="mt-4">Tambah Siswa</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item"><a href="/data-siswa">Dashboard</a></li>
+                            <li class="breadcrumb-item active">Tables</li>
+                        </ol>
+                        <form>
+                            <div class="form-group mb-3">
+                              <label for="exampleFormControlFile1">file</label>
+                              <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                            </div>
+                            <a class="btn btn-primary" href="data-siswa.html" role="button">Kirim</a>
+                          </form>
+                        
+                        
+                    </div>
+                </main>
+@endsection
