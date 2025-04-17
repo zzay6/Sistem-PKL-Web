f57eb175456b2cd61e40defe711799afd5bfedde
zzay <zacky29gaming@gmail.com>
2025-04-17 21:46:33 +0700

    Route

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,23 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 });
+
+Route::get('/perusahaan', function () {
+    return view('perusahaan');
+});
+
+Route::get('/data-siswa', function () {
+    return view('data-siswa');
+});
+
+Route::get('/admin', function () {
+    return view('admin');
+});
+
+Route::get('/lowongan', function () {
+    return view('lowongan');
+});
+
+Route::get('/lamaran', function () {
+    return view('lamaran');
+});
